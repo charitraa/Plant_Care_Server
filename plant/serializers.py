9a54ec3f3258a1fff222e7ed7cf2aa6fe5f5c9ad
charitra shrestha <stharabi9862187405@gmail.com>
2025-11-30 23:55:46 +0545

    update serializer of plants

--- plant/serializers.py
+++ plant/serializers.py
@@ -1,19 +1,12 @@
 from rest_framework import serializers
 from .models import Plant, WateringReminder
 
-class PlantSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Plant
-        fields = ["id", "plant_type", "image", "created_at"]
 
 class WateringReminderSerializer(serializers.ModelSerializer):
-    plant = PlantSerializer(read_only=True)
-
     class Meta:
         model = WateringReminder
         fields = [
             "id",
-            "plant",
             "temperature",
             "humidity",
             "sunlight",
@@ -23,3 +16,27 @@ class WateringReminderSerializer(serializers.ModelSerializer):
             "created_at",
         ]
         read_only_fields = ["watering_days", "next_watering_date", "created_at"]
+
+
+class PlantSerializer(serializers.ModelSerializer):
+    watering_days = serializers.SerializerMethodField()
+    next_watering_date = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Plant
+        fields = [
+            "id",
+            "plant_type",
+            "image",
+            "created_at",
+            "watering_days",
+            "next_watering_date",
+        ]
+
+    def get_watering_days(self, obj):
+        reminder = obj.reminders.order_by("-created_at").first()
+        return reminder.watering_days if reminder else None
+
+    def get_next_watering_date(self, obj):
+        reminder = obj.reminders.order_by("-created_at").first()
+        return reminder.next_watering_date if reminder else None
